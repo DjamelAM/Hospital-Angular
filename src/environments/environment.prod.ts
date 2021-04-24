6e3cb739c322af9186a516b124afda6cf250d240
Djamel <amraoui.djamel@gmail.com>
2021-04-24 21:43:11 +0200

    maj env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  doctorEndpoint: 'https://limitless-dawn-75015.herokuapp.com/doctors',
+  patientEndpoint: 'https://limitless-dawn-75015.herokuapp.com/patients',
 };
